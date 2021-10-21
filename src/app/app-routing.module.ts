1336bc848a033d42ac90d39c0c3e2a3312ab4a43
Palash Gupta <palgupta8@publicisgroupe.net>
2021-10-21 16:03:31 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path :'banner' , loadChildren : ()=> import('../app/components/banner/banner.module').then(m=> m.BannerModule)},
+   {path :'ecommerce' , loadChildren :()=> import('./components/eCommerce/e-commerce.module').then(m=>m.ECommerceModule)},
+   {path :'alert' , loadChildren :()=> import('./components/alert/alert.module').then(m=>m.AlertModule)},
+   {path : 'timer' , loadChildren :()=> import('./components/timer/timer.module').then(m=>m.TimerModule)},
+   {path : 'timer2' , loadChildren :()=> import('./components/timer-2/timer/timer2.module').then(m=>m.Timer2Module)},
+   {path : 'students' , loadChildren :()=> import('./components/studentsData/students.module').then(m=>m.StudentsModule)}
+   
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
